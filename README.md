aa6a22a81208ff8a575e62b9cac76c84fc01b87f
Martin Podloucký <podlouckymartin@gmail.com>
2022-06-23 10:38:21 +0200

    Basic markdown processing

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # czechisite
+
 Opinionated zero config static site generator for teaching purposes
+
+## Developing
+
+Run
+
+```
+npm run dev
+```
+
+Test the generator inside the `test` folder.
+
+```
+node ../dist/index.js example
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.info('it works');
+import { promises as fs } from 'fs';
+import path from 'path';
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+
+const baseDir = process.argv[2];
+const indexFile = path.join(baseDir, 'index.md');
+const content = await fs.readFile(indexFile, 'utf-8');
+
+const markdown = unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeSanitize)
+  .use(rehypeStringify);
+
+const vfile = await markdown.process(content);
+
+console.info(vfile.value);
